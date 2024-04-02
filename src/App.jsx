5b3d19634e6ab22f87cbcfda64607c17653ca984
dhwanisarokar <dhwanisarokar@gmail.com>
2024-04-02 23:39:30 +0530

    impliment class components of counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
 import "./App.css";
+import Counter from "./components/counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <Counter />
+    </>
+  );
 }
 
 export default App;
